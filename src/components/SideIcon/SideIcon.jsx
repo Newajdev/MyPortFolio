1d8ed2c4a485d8087d0dfd2112eb86d4d23f4554
newajDev <newaj.design@gmail.com>
2025-08-10 14:35:44 +0600

    all social url added to the icon

--- src/components/SideIcon/SideIcon.jsx
+++ src/components/SideIcon/SideIcon.jsx
@@ -1,4 +1,5 @@
-import { FaFacebook, FaGithub, FaInstagramSquare, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+import { FaFacebook, FaGithub, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
 
 
 const SideIcon = () => {
@@ -6,11 +7,16 @@ const SideIcon = () => {
         <div className="relative z-50">
             <div className="px-4 py-6 bg-base-gray flex-col inline-flex gap-5 text-3xl rounded-full fixed right-5
             top-[50%] -translate-y-2/4">
-                <FaGithub className="hover:text-base hover:cursor-pointer duration-500 "/>
-                <FaLinkedin className="hover:text-base hover:cursor-pointer duration-500 "/>
-                <FaFacebook className="hover:text-base hover:cursor-pointer duration-500 "/>
-                <FaTwitter className="hover:text-base hover:cursor-pointer duration-500 "/>
-                <FaInstagramSquare className="hover:text-base hover:cursor-pointer duration-500 "/>
+                <a href="https://github.com/Newajdev" target="_blank"><FaGithub className="hover:text-base hover:cursor-pointer duration-500 "/></a>
+                <a href="https://www.linkedin.com/in/muhammad-newaj" target="_blank"><FaLinkedin className="hover:text-base hover:cursor-pointer duration-500 "/></a>
+                <a href="https://www.facebook.com/muhammadshalenewaj" target="_blank"><FaFacebook className="hover:text-base hover:cursor-pointer duration-500 "/></a>
+                <a href="https://x.com/ms_newaj" target="_blank"><BsTwitterX className="hover:text-base hover:cursor-pointer duration-500 "/></a>
+                <a href="https://www.instagram.com/_mdnewaj_" target="_blank"><FaInstagramSquare className="hover:text-base hover:cursor-pointer duration-500 "/></a>
+    
+                
+                
+                
+                
             </div>
         </div>
     );
