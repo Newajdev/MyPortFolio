815b4cf584eef7dbd8b6053447e1612a5a3757d5
newajDev <newaj.design@gmail.com>
2025-08-12 05:01:53 +0600

    responsiveness added-2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,15 +5,15 @@ import { ReactTyped} from 'react-typed';
 
 const Header = () => {
     return (
-        <div className='h-[565px] lg:h-[779px]'>
+        <div className='h-[520px] lg:h-[779px]'>
             <div className=''>
                 <div className='relative'>
                     <h1 className='text-3xl lg:text-8xl font-semibold text-center lg:leading-24 mt-6 lg:mt-14 flex flex-col'><span>I’m <span className='text-base'>Newaj,</span></span>
                     <ReactTyped strings={['MERN Stack Developer']} typeSpeed={60} /></h1>
 
-                    <img className='absolute top-[-15px] lg:top-[-20px] right-[100px] lg:right-[380px] w-8 lg:w-10 rotate-180' src={Ornament} alt="" />
+                    <img className='absolute top-[-15px] lg:top-[-20px] right-[90px] lg:right-[380px] w-8 lg:w-10 rotate-180' src={Ornament} alt="" />
 
-                    <img className='absolute top-[50px] lg:top-[180px] left-[8px] lg:left-[30px] w-12 lg:w-20' src={Ornament} alt="" />
+                    <img className='absolute top-[58px] lg:top-[180px] left-[0px] lg:left-[30px] w-8 lg:w-20' src={Ornament} alt="" />
                 </div>
                 <div className='relative'>
                     <img className='absolute -top-4 lg:-top-16 right-2/4 translate-x-2/4' src={Profile} alt="" />
